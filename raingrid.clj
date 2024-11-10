85cb3a43d18652ad1ba6531f539564b28f5f4947
George Kontsevich <kxygk@aliyun.com>
2024-11-10 17:47:16 +0800

    Added: generating maps of pure vertical stripes

--- raingrid.clj
+++ raingrid.clj
@@ -5,14 +5,37 @@
 
 (defn
   gen
-  [^long width
-   ^long height
-   ^long type]
+  [width
+   height
+   type
+   & [{:keys [ver-offset
+              ver-stride
+              ver-stripe-width]
+       :or   {ver-offset       0
+              ver-stride       10
+              ver-stripe-width 1}}]]
   (let [image (java.awt.image.BufferedImage. width
                                              height
                                              java.awt.image.BufferedImage/TYPE_USHORT_GRAY)]
-    image))
-
+    (let [raster          (-> image
+                              .getRaster)
+          ver-white-array (int-array (* height
+                                        ver-stripe-width)
+                                     (* (Short/MAX_VALUE)
+                                        2))]
+      (->> (range ver-offset
+                  width
+                  ver-stride)
+           (run! (fn [x-start]
+                   (.setPixels raster
+                               x-start ;; weird function
+                               0 ;; takes an array of int/double/float
+                               ver-stripe-width  ;; but the underlying format is UShort
+                               height  ;; and seemingly 0-65535
+                               ver-white-array)))))
+    (-> image
+        (javax.imageio.ImageIO/write "tiff"
+                                     (java.io.File. "vertical-stripes.tiff")))))
 
 
 ;; IMERG data is given to us in GeoTIFF format.
@@ -35,3 +58,53 @@
 #_
 (gen-imerg)
 ;; => #object[java.awt.image.BufferedImage 0x22e7c455 "BufferedImage@22e7c455: type = 11 ColorModel: #pixelBits = 16 numComponents = 1 color space = java.awt.color.ICC_ColorSpace@66971d7a transparency = 1 has alpha = false isAlphaPre = false ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"]
+
+
+#_
+(-> (gen-imerg)
+    .getRaster)
+;; => #object[sun.awt.image.ShortInterleavedRaster 0x7c4cae87 "ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"]
+
+;; Default BufferImage values are `0.0` ie. `black`
+#_
+(-> (gen-imerg)
+    .getRaster
+    (.getPixel 0
+               0
+               nil))
+;; => [0.0]
+
+;; White square
+#_
+(let [buff (gen-imerg)]
+  (-> buff
+      .getRaster
+      (.setPixels 1000 ;; weird function
+                  1000 ;; takes an array of int/double/float
+                  200  ;; but the underlying format is UShort
+                  200  ;; and seemingly 0-65535
+                  (int-array 40000 (* (Short/MAX_VALUE)
+                                      2)))) ;; larger numbers get rounded to 65535
+  (-> buff
+      (javax.imageio.ImageIO/write "tiff"
+                                   (java.io.File. "white-square.tiff"))))
+
+
+
+
+(double-array 10)
+
+#_
+(-> (gen-imerg)
+    (javax.imageio.ImageIO/write "tiff"
+                                 (java.io.File. "test-out.tiff")))
+
+(->  "./imerg-sample.tif"
+     java.io.File.
+     javax.imageio.ImageIO/read
+     (javax.imageio.ImageIO/write "tiff"
+                                  (java.io.File. "reincoded.tiff")))
+;; Output images are rather large because I'm not setting any compression
+;; Original images have LZW compression
+;; See:
+;; file:///usr/share/doc/openjdk-21-jre-headless/api/java.desktop/javax/imageio/metadata/doc-files/tiff_metadata.html#Compression
