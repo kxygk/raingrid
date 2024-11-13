584338a7f2bfa021d3afa8c5dbe1634462c78f64
George Kontsevich <kxygk@aliyun.com>
2024-11-13 17:57:45 +0800

    Added: stable seed + filter to image range
    I'm still getting weird "blue" values in the images
    I thought my random values were blowing the range,
    but it seems the issue is elsewhere

--- raingrid.clj
+++ raingrid.clj
@@ -1,134 +1,8 @@
 (ns raingrid
   "Functionality for creating fake IMERG data"
-  (:use ;;geogrid
-   geoprim))
+  (:use geoprim)
+  (:require fastmath.random))
 
-(defn
-  gen
-  [width
-   height
-   type
-   & [{:keys [background-noise-mean
-              pattern-noise-mean
-              pattern-noise-var-scaling-fac
-              ver-offset
-              ver-stride
-              ver-stripe-width
-              hor-offset
-              hor-stride
-              hor-stripe-width]
-       :or   {background-noise-mean 5000.0
-              pattern-noise-mean (* Short/MAX_VALUE
-                                    0.5)
-              pattern-noise-var-scaling-fac 0.2
-              ver-offset       0
-              ver-stride       7
-              ver-stripe-width 2
-              hor-offset       0
-              hor-stride       19
-              hor-stripe-width 3}}]]
-  (let [image (java.awt.image.BufferedImage. width
-                                             height
-                                             java.awt.image.BufferedImage/TYPE_USHORT_GRAY)]
-    (let [raster               (-> image
-                                   .getRaster)
-          background-dist      (fastmath.random/distribution :gamma {:shape 1.0 ;; keep exponential
-                                                                     :scale background-noise-mean})
-          pattern-dist         (fastmath.random/distribution :normal {:mu pattern-noise-mean
-                                                                      :sd (* pattern-noise-mean
-                                                                             pattern-noise-var-scaling-fac)})
-          total-field-size     (* width
-                                  height)
-          ver-white-array-size (* height
-                                  ver-stripe-width)
-          hor-white-array-size (* width
-                                  hor-stripe-width)]
-      ;; Fill field with gamma
-      (.setPixels raster
-                  0
-                  0 ;; takes an array of int/double/float
-                  width  ;; but the underlying format is UShort
-                  height  ;; and seemingly 0-65535
-                  (int-array total-field-size
-                             (->> background-dist
-                                  fastmath.random/->seq
-                                  (filter pos?))))
-      ;; Vertical stripes
-      (->> (range ver-offset
-                  width
-                  ver-stride)
-           (run! (fn [x-start]
-                   (.setPixels raster
-                               x-start ;; weird function
-                               0 ;; takes an array of int/double/float
-                               ver-stripe-width  ;; but the underlying format is UShort
-                               height  ;; and seemingly 0-65535
-                               (int-array ver-white-array-size
-                                          (map +
-                                               (->> pattern-dist
-                                                    fastmath.random/->seq
-                                                    (filter pos?))#_
-                                               (->> background-dist
-                                                    fastmath.random/->seq
-                                                    (filter pos?))))))))
-      ;; Horizontal stripes
-      (->> (range hor-offset
-                  height
-                  hor-stride)
-           (run! (fn [y-start]
-                   (.setPixels raster
-                               0
-                               y-start
-                               width
-                               hor-stripe-width
-                               (int-array hor-white-array-size
-                                          (map +
-                                               (->> pattern-dist
-                                                    fastmath.random/->seq
-                                                    (filter pos?))
-                                               (->> background-dist
-                                                    fastmath.random/->seq
-                                                    (filter pos?)))))))))
-    (-> image
-        (javax.imageio.ImageIO/write "tiff"
-                                     (java.io.File. "vertical-stripes.tiff")))))
-
-
-;; IMERG data is given to us in GeoTIFF format.
-;; We inspect the format to see its encoding
-#_
-(->  "./imerg-sample.tif"
-     java.io.File.
-     javax.imageio.ImageIO/read
-     .toString)
-;; => "BufferedImage@a3ca079: type = 11 ColorModel: #pixelBits = 16 numComponents = 1 color space = java.awt.color.ICC_ColorSpace@66971d7a transparency = 1 has alpha = false isAlphaPre = false ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"
-
-(defn
-  gen-imerg
-  "Generate a fake image based on the IMERG params"
-  []
-  (gen 3600
-       1800
-       java.awt.image.BufferedImage/TYPE_USHORT_GRAY))
-
-#_
-(gen-imerg)
-;; => #object[java.awt.image.BufferedImage 0x22e7c455 "BufferedImage@22e7c455: type = 11 ColorModel: #pixelBits = 16 numComponents = 1 color space = java.awt.color.ICC_ColorSpace@66971d7a transparency = 1 has alpha = false isAlphaPre = false ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"]
-
-
-#_
-(-> (gen-imerg)
-    .getRaster)
-;; => #object[sun.awt.image.ShortInterleavedRaster 0x7c4cae87 "ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"]
-
-;; Default BufferImage values are `0.0` ie. `black`
-#_
-(-> (gen-imerg)
-    .getRaster
-    (.getPixel 0
-               0
-               nil))
-;; => [0.0]
 
 ;; White square
 #_
@@ -145,22 +19,156 @@
       (javax.imageio.ImageIO/write "tiff"
                                    (java.io.File. "white-square.tiff"))))
 
+;; Note:
+;; Default BufferImage values are `0.0` ie. `black`
 
-
-
-(double-array 10)
-
+;; IMERG data is given to us in GeoTIFF format.
+;; We inspect the format to see its encoding
 #_
-(-> (gen-imerg)
-    (javax.imageio.ImageIO/write "tiff"
-                                 (java.io.File. "test-out.tiff")))
-
 (->  "./imerg-sample.tif"
      java.io.File.
      javax.imageio.ImageIO/read
-     (javax.imageio.ImageIO/write "tiff"
-                                  (java.io.File. "reincoded.tiff")))
-;; Output images are rather large because I'm not setting any compression
-;; Original images have LZW compression
-;; See:
-;; file:///usr/share/doc/openjdk-21-jre-headless/api/java.desktop/javax/imageio/metadata/doc-files/tiff_metadata.html#Compression
+     .toString)
+;; => "BufferedImage@a3ca079: type = 11 ColorModel: #pixelBits = 16 numComponents = 1 color space = java.awt.color.ICC_ColorSpace@66971d7a transparency = 1 has alpha = false isAlphaPre = false ShortInterleavedRaster: width = 3600 height = 1800 #numDataElements 1"
+
+
+
+;; Setting seeds for random variables is done in this way
+#_
+(let [seed 69]
+  (->> (fastmath.random/distribution :gamma {:rng (fastmath.random/rng :jdk seed)})
+       fastmath.random/->seq
+       (take 10)))
+;; With `nil` the values become "random" again
+#_
+(let [seed nil]
+  (->> (fastmath.random/distribution :gamma {:rng (fastmath.random/rng :jdk seed)})
+       fastmath.random/->seq
+       (take 10)))
+
+
+(defn
+  gen
+  [width
+   height
+   & [{:keys [background-noise-mean
+              pattern-noise-mean
+              pattern-noise-var-scaling-fac
+              ver-offset
+              ver-stride
+              ver-stripe-width
+              hor-offset
+              hor-stride
+              hor-stripe-width
+              seed]
+       :or   {background-noise-mean         5000.0
+              pattern-noise-mean            (* Short/MAX_VALUE
+                                               0.5)
+              pattern-noise-var-scaling-fac 0.2
+              ver-offset                    0
+              ver-stride                    7
+              ver-stripe-width              2
+              hor-offset                    0
+              hor-stride                    19
+              hor-stripe-width              3
+              seed                          nil}}]]
+  (let [image (java.awt.image.BufferedImage. width
+                                             height
+                                             java.awt.image.BufferedImage/TYPE_USHORT_GRAY)]
+    (let [raster               (-> image
+                                   .getRaster)
+          background-dist      (fastmath.random/distribution :gamma {:shape 1.0 ;; keep exponential
+                                                                     :scale background-noise-mean
+                                                                     :rng   (fastmath.random/rng :jdk seed)})
+          pattern-dist         (fastmath.random/distribution :normal {:mu  pattern-noise-mean
+                                                                      :sd  (* pattern-noise-mean
+                                                                              pattern-noise-var-scaling-fac)
+                                                                      :rng (fastmath.random/rng :jdk seed)})
+          total-field-size     (* width
+                                  height)
+          ver-white-array-size (* height
+                                  ver-stripe-width)
+          hor-white-array-size (* width
+                                  hor-stripe-width)]
+      ;; Fill field with gamma noise
+      (.setPixels raster
+                  0
+                  0 ;; takes an array of int/double/float
+                  width  ;; but the underlying format is UShort
+                  height  ;; and seemingly 0-65535
+                  (int-array total-field-size
+                             (->> background-dist
+                                  fastmath.random/->seq
+                                  (filter pos?))))
+      ;; Vertical stripes (gamme + normal)
+      (->> (range ver-offset
+                  width
+                  ver-stride)
+           (run! (fn [x-start]
+                   (.setPixels raster
+                               x-start           ; weird function
+                               0                 ; takes an array of int/double/float
+                               ver-stripe-width  ; but the underlying format is UShort
+                               height            ; and seemingly 0-65535
+                               (int-array ver-white-array-size
+                                          (map +
+                                               (->> pattern-dist
+                                                    fastmath.random/->seq
+                                                    (filter pos?))
+                                               (->> background-dist
+                                                    fastmath.random/->seq
+                                                    (filter pos?))))))))
+      ;; Horizontal stripes
+      (->> (range hor-offset
+                  height
+                  hor-stride)
+           (run! (fn [y-start]
+                   (.setPixels raster
+                               0
+                               y-start
+                               width
+                               hor-stripe-width
+                               (int-array hor-white-array-size
+                                          (filter #(and (> %
+                                                           0.0)
+                                                        (> (* Short/MAX_VALUE
+                                                              2.0)
+                                                           %))
+                                                  (map +
+                                                       (->> pattern-dist
+                                                            fastmath.random/->seq
+                                                            (filter pos?))
+                                                       (->> background-dist
+                                                            fastmath.random/->seq
+                                                            (filter pos?))))))))))
+    image))
+
+
+(defn
+  spit-rainlike
+  "Spit to file based on the pattern noise mean level
+  Output images are rather large because I'm not setting any compression
+  Original images have LZW compression
+  See:
+  file:///usr/share/doc/openjdk-21-jre-headless/api/java.desktop/javax/imageio/metadata/doc-files/tiff_metadata.html#Compression
+  "
+  [pattern-level]
+  (-> (gen 3600
+           1800
+           {:pattern-noise-mean pattern-level
+            :seed               pattern-level})
+      (javax.imageio.ImageIO/write "tiff"
+                                   (java.io.File. (str "/home/kxygk/Projects/raingrid/out/noise"
+                                                       (format "%07d"
+                                                               pattern-level)
+                                                       ".tiff")))))
+
+#_
+(spit-rainlike 8000)
+#_
+(->> (range 10
+            9000
+            60)
+     (run! spit-rainlike))
+
+
